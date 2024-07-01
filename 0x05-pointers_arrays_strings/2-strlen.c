041d784c6f6db10aeeff888a55a866e36f522be0
PascalAmah <pascal.amah10@gmail.com>
2024-07-01 12:57:29 -0700

    Commit for a function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,15 +6,16 @@
  *
  * Return the length of the string
  */
-
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
-	while (s[i] != '\0')
+
+	while(s[i] != '\0')
 	{
 		i++;
 	}
-	
-	return (i);
+
+	return(i);
 }
