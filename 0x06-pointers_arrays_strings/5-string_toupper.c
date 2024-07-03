78aa0742ebaa535075b499c103ce98fb7da3ff6e
PascalAmah <pascal.amah10@gmail.com>
2024-07-03 14:40:47 -0700

    Commit for a function that changes all lowercase letters of string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,11 @@
 char *string_toupper(char *s)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
+		if (s[i] >= 'a' && s[i] < 'z')
 			s[i] = s[i] - 'a' + 'A';
-		}
 	}
 	return (s);
 }
