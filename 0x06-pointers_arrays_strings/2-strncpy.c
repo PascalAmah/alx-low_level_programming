c81e177f4559256b8b8f411849e8b4b4629bdf68
PascalAmah <pascal.amah10@gmail.com>
2024-07-03 13:56:10 -0700

    Commit for a function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+
+	while (n > 0)
+	{
+		*ptr = '\0';
+		ptr++;
+		n--;
+	}
+
+	return dest;
+}
