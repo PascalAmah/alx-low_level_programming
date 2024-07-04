6a803e713dd51bd47e208761755d9afe3c8064f3
PascalAmah <pascal.amah10@gmail.com>
2024-07-04 09:06:03 -0700

    Commit for a program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,25 +2,23 @@
 
 int main(void)
 {
-    int n;
-    int a[5];
-    int *p;
+	int n;
+	int a[5];
+	int *p;
 
-    a[2] = 1024;
-    p = &n;
-    /*
-     * write your line of code here...
-     * Remember:
-     * - you are not allowed to use the variable a
-     * - you are not allowed to modify the variable p
-     * - you can only write one statement
-     * - you are not allowed to use ,
-     * - you are not allowed to code anything else than the line of expected line of code at the expected line
-     * - do not remove anything from the initial code (not even the comments)
-     * - don’t change anything but the line of code you are adding (don’t change the spaces to tabs!)
-     * - you are allowed to use the standard library
-     */
-    *(p + 5) = 98;
-    printf("a[2] = %d\n", a[2]);
-    return (0);
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * write your line of code here
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
 }
