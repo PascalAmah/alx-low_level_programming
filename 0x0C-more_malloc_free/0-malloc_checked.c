73271decbde54c087e2aa621898d6dd7121356d4
PascalAmah <pascal.amah10@gmail.com>
2024-07-18 17:36:38 -0700

    Commit for a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,16 +3,18 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to be allocated
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
+
 	ptr = malloc(b);
 
-		if (ptr == NULL)
-			exit(98);
+	if (ptr == NULL)
+		exit(98);
+
 	return (ptr);
 }
